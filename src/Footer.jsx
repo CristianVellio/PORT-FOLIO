d18d25eda0fa61ce812b4aac94ca66ec0da519b2
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-07-25 02:45:19 -0300

    fixed footer responisvness

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,44 +4,44 @@ import img from "./assets/LOGO.png";
 
 function Footer() {
   return (
-    <footer className="bg-orange-700 text-white lg:px-48 py-20">
-      <div className="container flex justify-between items-center mx-auto gap-4">
+    <footer className="bg-orange-700 text-white px-4 py-8 md:px-12 lg:px-48">
+      <div className="container flex flex-col lg:flex-row justify-between items-center mx-auto gap-4">
         <img
           src={img}
           width={80}
           height={80}
-          className="rounded-full"
+          className="rounded-full mb-4 lg:mb-0"
           alt="logo"
         />
-        <div className="mb-4 md:md-0">
-          <p className="text-16px text-bold my-4">Cristian Vellio© 2024</p>
+        <div className="text-center lg:text-left mb-4 lg:mb-0">
+          <p className="text-lg font-bold">Cristian Vellio© 2024</p>
         </div>
-        <div>
-          <h3 className="text-3xl font-semibold text-lime-400 py-2 uppercase">
+        <div className="text-center lg:text-left mb-4 lg:mb-0">
+          <h3 className="text-xl font-semibold text-lime-400 py-2 uppercase">
             Formation
           </h3>
-          <ul className="text-[16px] text-bold my-4">
+          <ul className="text-lg font-bold">
             <li className="my-2">Full-Stack Developer</li>
             <li className="my-2">Web Developer</li>
             <li className="my-2">Python Developer</li>
           </ul>
         </div>
-        <div className="flex flex-col md:mb-0">
-          <h3 className="text-3xl font-semibold text-lime-400 py-2 uppercase">
+        <div className="text-center lg:text-left">
+          <h3 className="text-xl font-semibold text-lime-400 py-2 uppercase">
             Find Me on
           </h3>
-          <div className="flex space-x-2 flex-row items-center justify-items-center">
+          <div className="flex justify-center lg:justify-start space-x-4">
             <a
               href="https://www.github.com/CristianVellio"
-              className="text-white hover:text-lime-400 rounded-full p-2"
+              className="text-white hover:text-lime-400 p-2"
             >
-              <FaGithub className="text-[48px]" />
+              <FaGithub className="text-3xl" />
             </a>
             <a
               href="https://www.linkedin.com/in/cristianvellio"
-              className="text-white hover:text-lime-400 rounded-full p-2"
+              className="text-white hover:text-lime-400 p-2"
             >
-              <FaLinkedinIn className="text-[48px]" />
+              <FaLinkedinIn className="text-3xl" />
             </a>
           </div>
         </div>
