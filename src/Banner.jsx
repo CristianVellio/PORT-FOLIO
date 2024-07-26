898e5cce8519530a298598e83c881d4f913559d3
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2024-07-26 01:55:38 -0300

    Fixed/Layout-Text/Banner

--- src/Banner.jsx
+++ src/Banner.jsx
@@ -22,47 +22,44 @@ function Banner() {
       duration: 750,
     });
   }, []);
+
   return (
     <div
       id="Banner"
-      className="lg:px-56 px-10 lg:py-0 py-20 text-center gap-5 lg-text-start flex lg:flex-row flex-col-reverse justify-between lg:gap-20 items-center"
+      className="lg:px-56 px-10 lg:py-0 py-20 text-center lg:text-left gap-5 flex lg:flex-row flex-col-reverse justify-between items-center"
     >
-      <div className="h-full lg:py-40 flex flex-col justify-center lg:items-start items-center text-center text-white">
+      <div className="h-full lg:py-40 flex flex-col justify-center items-center lg:items-start text-center lg:text-left text-white">
         <h1
           data-aos="fade-right"
           className="text-[52px] font-semibold mb-8 leading-normal uppercase"
         >
-          Cristian Vellio <br></br>
+          Cristian Vellio <br />
           <span className="text-lime-500">{text}</span>
           <span className="text-lime-500">
             <Cursor cursorStyle="|" />
           </span>
-          <br></br>
+          <br />
           <span className="text-lime-500"> Developer</span>
         </h1>
         <p
           data-aos="fade-left"
-          className="text-[30px] font-semibold mb-8 leading-normal text-center uppercase"
+          className="text-[30px] font-semibold mb-8 leading-normal uppercase"
         >
           HTML | CSS | JavaScript | React | Python
         </p>
-        <div className="flex mt-8 gap-2">
-          <div className="flex items-center justify-center">
-            <div className="flex space-x-2">
-              <a
-                href="https://www.github.com/CristianVellio"
-                className="text-lime-600 hover:text-lime-500 rounded-full glow p-2"
-              >
-                <FaGithub className="text-[48px]" />
-              </a>
-              <a
-                href="https://www.linkedin.com/in/cristianvellio"
-                className="text-lime-600 hover:text-lime-500 rounded-full glow p-2"
-              >
-                <FaLinkedinIn className="text-[48px]" />
-              </a>
-            </div>
-          </div>
+        <div className="flex mt-8 gap-4 justify-center lg:justify-start">
+          <a
+            href="https://www.github.com/CristianVellio"
+            className="text-lime-600 hover:text-lime-500 rounded-full glow p-2"
+          >
+            <FaGithub className="text-[48px]" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/cristianvellio"
+            className="text-lime-600 hover:text-lime-500 rounded-full glow p-2"
+          >
+            <FaLinkedinIn className="text-[48px]" />
+          </a>
         </div>
       </div>
       <img
