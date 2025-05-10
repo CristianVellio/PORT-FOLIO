d5b15634eb2bce006476b3003bc475ec046def56
CristianVellio <160356772+CristianVellio@users.noreply.github.com>
2025-05-10 02:25:51 -0300

    fiz-swiper-ratio/cert

--- src/Certifications.jsx
+++ src/Certifications.jsx
@@ -99,9 +99,9 @@ function Certifications() {
           pagination={{ clickable: true }}
           breakpoints={{
             480: { slidesPerView: 1, spaceBetween: 20 },
-            640: { slidesPerView: 2, spaceBetween: 20 },
-            768: { slidesPerView: 3, spaceBetween: 30 },
-            1024: { slidesPerView: 4, spaceBetween: 40 },
+            640: { slidesPerView: 1.5, spaceBetween: 20 },
+            768: { slidesPerView: 2, spaceBetween: 30 },
+            1024: { slidesPerView: 3, spaceBetween: 40 },
           }}
           modules={[Pagination, Navigation]}
           className="mySwiper"
